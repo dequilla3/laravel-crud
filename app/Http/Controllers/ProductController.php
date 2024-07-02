c4ad48364c31a8d23be02acf5065ba18a5169cf8
Kim Ariel Dequilla <deqkim4@gmail.com>
2024-07-03 03:01:40 +0800

    feat: added create

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return view('products.index');
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'qty' => 'required|numeric', // Use 'numeric' for numeric fields
+            'description' => 'nullable',
+        ]);
+
+        $newProduct = Product::create($data);
+        return redirect()->route('product.index');
+    }
+}
---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Create a product</h1>
+    <form method="post" action="{{ route('product.store') }}">
+        @csrf
+        @method('post')
+        <div>
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" placeholder="name">
+        </div>
+        <div>
+            <label for="qty">Qty</label>
+            <input type="text" name="qty" id="qty" placeholder="qty">
+        </div>
+        <div>
+            <label for="price">Price</label>
+            <input type="text" name="price" id="price" placeholder="price">
+        </div>
+        <div>
+            <label for="description">Description</label>
+            <input type="text" name="description" id="description" placeholder="Description">
+        </div>
+        <div><input type="submit" value="Save" />
+    </form>
+</body>
+
+</html>
